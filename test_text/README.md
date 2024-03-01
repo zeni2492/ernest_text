b4824f647e298d9fbf3892a596b23c22520557e7
zeni2492 <mr.sher123@mail.ru>
2024-03-02 03:09:19 +0500

    Added_mediaQueries

--- test_text/README.md
+++ test_text/README.md
@@ -5,3 +5,13 @@ This template should help get you started developing with Vue 3 in Vite. The tem
 ## Recommended IDE Setup
 
 - [VS Code](https://code.visualstudio.com/) + [Volar](https://marketplace.visualstudio.com/items?itemName=Vue.volar) (and disable Vetur) + [TypeScript Vue Plugin (Volar)](https://marketplace.visualstudio.com/items?itemName=Vue.vscode-typescript-vue-plugin).
+
+## Как все работает
+Для начала имеются 4 компонента где в каждом находится одно четверостишье с целью более удобного взаимодействия с каждым четверостишьем отдельно.
+
+вся логика находится в файле main_text.vue (src/components/main_text.vue)
+первая функция это эффект нажатия кнопки происходит путем добавления класса к стилям кнопки путем изменения булевой переменной и внесения в ее аргументы индекса элемента  this.isActive[buttonNumber] = !this.isActive[buttonNumber];
+и таким же образом происходит отображение компонентов только при помощи директивы v-if для избежания изменения стилей.
+
+Затем идет функция которая берет объект isActive с булевыми данными и фильтрует их по количеству истинных значений и возвращает длинну объекта. Если количество активных компонентов меньше 3 или если компонент, который собираются переключить, в  активен, то функция переключает видимость компонента. в ином случае убрать компонент нельзя.
+
--- test_text/src/components/main_text.vue
+++ test_text/src/components/main_text.vue
@@ -1,23 +1,33 @@
 <template>
-    <header class="title">Invictus</header>
-    <main class="main-container">
-        <span class="buttons-container">
-            <button class="quatrain-controller first-quatrain-button"></button>
-            <button class="quatrain-controller second-quatrain-button"></button>
-            <button class="quatrain-controller third-quatrain-button"></button>
-            <button class="quatrain-controller fourth-quatrain-button"></button>
-        </span>
-        <div class="quatrain ">
-            <first_quatrain />
-        </div >
-        <div class="quatrain ">
-            <second_quatrain />
-        </div >
-        <div class="quatrain ">
-            <third_quatrain />
+    <header>
+        <div class="title">
+            <h1 class="title__name">Invictus</h1>
         </div>
-        <div class="quatrain ">
-            <fourth_quatrain />
+        <div class="author">
+            <h2 class="author__name">By William Ernest Henley</h2>
+        </div>
+    </header>
+
+    <main class="main_text">
+        <nav class="buttons">
+            <button @click="toggleActive(1); toggleComponent(1)" :class="[{'active': isActive[1]},'button_controller']"></button>
+            <button @click="toggleActive(2); toggleComponent(2)" :class="[{'active': isActive[2]},'button_controller']"></button>
+            <button @click="toggleActive(3); toggleComponent(3)" :class="[{'active': isActive[3]},'button_controller']"></button>
+            <button @click="toggleActive(4); toggleComponent(4)" :class="[{'active': isActive[4]},'button_controller']"></button>
+        </nav>
+        <div class="quatrain_container">
+            <div v-if="showComponent[1]" class="first_quatrain">
+                <first_quatrain/>
+            </div>
+            <div v-if="showComponent[2]" class="second_quatrain">
+                <second_quatrain/>
+            </div>
+            <div v-if="showComponent[3]" class="third_quatrain">
+                <third_quatrain/>
+            </div>
+            <div v-if="showComponent[4]" class="fourth_quatrain">
+                <fourth_quatrain/>
+            </div>
         </div>
     </main>
 </template>
@@ -28,45 +38,193 @@ import second_quatrain from './quatrains/second_quatrain.vue';
 import third_quatrain from './quatrains/third_quatrain.vue';
 import fourth_quatrain from './quatrains/fourth_quatrain.vue';
 export default {
+    data() {
+        return {
+            isActive: {
+                1: false,
+                2: false,
+                3: false,
+                4: false
+            },
+            showComponent:{
+                1: true,
+                2: true,
+                3: true,
+                4: true
+            }
+        }
+    },
     components: {
         first_quatrain,
         second_quatrain,
         third_quatrain,
         fourth_quatrain
+    },
+    methods: {
+        DontHideElementWhenItLast(){
+            return Object.values(this.isActive).filter(Boolean).length;
+        },
+        toggleComponent(ComponentNumber) {
+           if(this.DontHideElementWhenItLast() < 3 || this.isActive[ComponentNumber]){
+               this.showComponent[ComponentNumber] = !this.showComponent[ComponentNumber];
+           }
+        },
+        toggleActive(buttonNumber) {
+           if(this.DontHideElementWhenItLast() < 3 || this.isActive[buttonNumber]){
+            this.isActive[buttonNumber] = !this.isActive[buttonNumber];
+           }
+        }
     }
 }
 </script>
 
 <style>
+header {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    position: relative;
+}
+
 .title {
-    color: black;
-    font-weight: 500;
-    font-size: 50px;
+    margin-right: 50px;
     text-align: center;
 }
 
-.quatrain-controller{
+.author {
+    position: absolute;
+    right: 30%;
+    top: 50%;
+    transform: translateY(-50%);
+}
+.author__name {
+    font-size: 15px;
+}
+
+.main_text {
+    width: 100%;
+    display: flex;
+    justify-content: center;
     position: relative;
-    background-color: black;
-    border: none;
+}
+
+.quatrain_container{
+    display: flex;
+    flex-direction: column;
+    align-items: center;
+    justify-content: center;
+    width: 500px;
+    text-align: center;
+}
+
+.buttons{
+    position: absolute;
+    margin-top: 30px;
+    left: 20%;
+}
+
+.button_controller{
     width: 20px;
     height: 20px;
-    margin-left: 15px;
+    margin: 5px;
+    border: 2px solid black;
+    background: #000;
     cursor: pointer;
 }
 
-.main-container {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    margin-top: 10px;
-    width: 300px;
-    text-align: center;
+.button_controller.active{
+    background: #00000015;
 }
 
-h2 {
-    color: black;
-    font-weight: 500;
-    font-size: 30px;
+@media (min-width: 450px) and (max-width: 1000px) {
+    .author{
+        right: 4%;
+    }
+    
+}
+
+@media (min-width: 450px) and (max-width: 1200px) {
+    .title{
+        text-align: center;
+
+    }
+    .author{
+        right: 4%;
+    }
+    .quatrain_container{
+        margin-top: 50px;
+    }
+
+    .buttons{
+        left: 50%;
+        margin-top: 5px;
+        transform: translateX(-50%);
+    }
+}
+
+@media (max-width: 450px) {
+    .title{
+       text-align: center;
+    }
+    .author{
+        top: 50px;
+        right: 50%;
+        transform: translateX(50%);
+        text-wrap: nowrap;
+    }
+    .quatrain_container{
+        margin-top: 50px;
+    }
+
+    .buttons{
+        left: 50%;
+        margin-top: 5px;
+        transform: translateX(-50%);
+    }
+}
+
+@media (min-width: 1400px) {
+    .title{
+        text-align: center;
+    }
+    .title__name{
+        font-size: 60px;
+    }
+    .author__name{
+        font-size: 35px;
+    }
+    .author{
+        right: 5%;
+    }
+    h2{
+        font-size: 40px;
+    }
+    .button_controller{
+        width: 40px;
+        height: 40px;
+    }
+    .buttons{
+        left: 15%;
+    }
+    
+}
+
+@media (min-width: 2200px) {
+    .author{
+        right: 25%;
+    }
+
+    .buttons{
+        left: 25%;
+    }
+}
+
+@media (min-width: 4000px) {
+    .author{
+        right: 35%;
+    }
+    .buttons{
+        left: 35%;
+    }
 }
 </style>
